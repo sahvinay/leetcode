474904c756d68096abf9f95fded9be8d8f45a1d1
Vinay Sah <sonumpsah1@gmail.com>
2026-07-13 19:52:33 +0530

    Replace cycle detection with happy number logic version: 2
    Refactored the cycle detection algorithm to implement a happy number check instead.

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -1,3 +1,4 @@
+# Solution version: 2
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -5,24 +6,21 @@
 #         self.next = None
 
 class Solution:
-    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = fast = head
+    def isHappy(self, n: int) -> bool:
+        def square_digit(num: int) -> int:
+            square_num = 0
+            while num:
+                digit = num % 10
+                square_num += digit * digit
+                num //= 10
+            return square_num
 
-        # Step 1: Detect cycle
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+        slow = n
+        fast = n
+
+        while True:
+            slow = square_digit(slow)
+            fast = square_digit(square_digit(fast))
 
             if slow == fast:
-                break
-        else:
-            return None
-
-        # Step 2: Find cycle start
-        slow = head
-
-        while slow != fast:
-            slow = slow.next
-            fast = fast.next
-
-        return slow
+                return slow == 1
